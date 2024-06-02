bc66884c23ea2c658f4e7bbd43d1f3e028794409
odemola662 <odemola662@gmail.com>
2024-06-02 22:36:30 +0100

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,15 @@
+// // function validateForm() {
+// //     let x = document.forms ["My Form"]
+// //     ["fname"].value;
+// //     if(x == "") {
+// //         alert( "Name must be filled out");
+// //         return false;
+// //     }
+// // }
+
+// document.getElementById('contactForm').addEventListener('submit', function(event)
+// {
+// event.preventDefault();
+
+// alert('Fake Form submitted successfully!');
+// });
